bc9c56365f59aec5ad4721443ba580e511c79192
martin <martin@konfuzzyus.de>
2022-09-13 03:28:43 +0200

    reformated code

--- A3AEventMissionBase/functions.hpp
+++ A3AEventMissionBase/functions.hpp
@@ -1,70 +1,69 @@
-class CfgFunctions {
-    class FFPP
+
+class FFPP
+{
+    class Punishment
     {
-        class Punishment
-        {
-            class punishment_dataGet {};
-            class punishment_dataNamespace {};
-            class punishment_dataRem {};
-            class punishment_dataSet {};
-            class punishment_FF_addEH {};
-            class punishment_FF {};
-            class punishment {};
-        };
-        class UI
-        {
-            class customHint {};
-        };
-        class Utility
-        {
-            class getAdmin {};
-            class log {};
-        };
+        class punishment_dataGet {};
+        class punishment_dataNamespace {};
+        class punishment_dataRem {};
+        class punishment_dataSet {};
+        class punishment_FF_addEH {};
+        class punishment_FF {};
+        class punishment {};
     };
-
-    class HR
+    class UI
     {
-        class Create
-        {
-            class LGAirstrike {};
-        };
+        class customHint {};
+    };
+    class Utility
+    {
+        class getAdmin {};
+        class log {};
+    };
+};
 
-        class Players
-        {
-            class dressUp {};
-        };
-
-        class UI
-        {
-            class customHint {};
-        };
-
-        class Utility
-        {
-            class ValidateObjects {};
-        };
+class HR
+{
+    class Create
+    {
+        class LGAirstrike {};
     };
 
-    class A3A{
-        class AI{
-            class disableLayerAI {};
-        };
+    class Players
+    {
+        class dressUp {};
     };
 
-    class TS {
-        class spawnEnemy {
-            class SpawnInfantryOnMarker{};
-        };
-        class Detectors{
-            class detectMods{};
-            class infantryDetector{};
-        };
-        class Init {
-            class enemyUnits{};
-            class init_improvised_vehicles{};
-        };
-        class AI {
-            class disableAutonomousModePermanently{};
-        }
+    class UI
+    {
+        class customHint {};
     };
-}
+
+    class Utility
+    {
+        class ValidateObjects {};
+    };
+};
+
+class A3A{
+    class AI{
+        class disableLayerAI {};
+    };
+};
+
+class TS {
+    class spawnEnemy {
+        class SpawnInfantryOnMarker{};
+    };
+    class Detectors{
+        class detectMods{};
+        class infantryDetector{};
+    };
+    class Init {
+        class enemyUnits{};
+        class init_improvised_vehicles{};
+    };
+    class AI {
+        class disableAutonomousModePermanently{};
+    }
+};
